53669019c6da83f2ceb98c417502dc68f196d94a
ben <ben.alley@gmail.com>
2017-04-18 17:06:54 -0700

    ActionBarActivity to AppCompatActity for MovieInfoActivity

--- app/src/main/java/com/example/ben/movieapp/MovieInfoActivity.java
+++ app/src/main/java/com/example/ben/movieapp/MovieInfoActivity.java
@@ -1,25 +1,11 @@
 package com.example.ben.movieapp;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-public class MovieInfoActivity extends ActionBarActivity {
+public class MovieInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
