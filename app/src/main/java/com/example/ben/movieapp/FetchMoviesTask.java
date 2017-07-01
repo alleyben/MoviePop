06ffca30c1034887bccdfa5074f3acaa6983388a
ben <ben.alley@gmail.com>
2017-07-01 12:25:51 -0700

    changed FetchMovieTask third param to double level string array and took out string splitter

--- app/src/main/java/com/example/ben/movieapp/FetchMoviesTask.java
+++ app/src/main/java/com/example/ben/movieapp/FetchMoviesTask.java
@@ -15,13 +15,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+public class FetchMoviesTask extends AsyncTask<String, Void, String[][]> {
 
     private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
     private MoviesAdapter mMovieAdapter;
 
-    private String[] getMovieDataFromJson(String movieJsonStr) throws JSONException {
+    private String[][] getMovieDataFromJson(String movieJsonStr) throws JSONException {
 
         final String RESULTS = "results";
         final String TITLE = "title";
@@ -35,7 +35,7 @@ public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
         JSONObject movieJson = new JSONObject(movieJsonStr);
         JSONArray movieArray = movieJson.getJSONArray(RESULTS);
 
-        String[] resultStrArr = new String[movieArray.length()];
+        String[][] resultStrArr = new String[movieArray.length()][];
 
         for(int i = 0; i < resultStrArr.length; i++) {
 
@@ -46,48 +46,41 @@ public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
             String movieRating = movieInfo.getString(SCORE);
             String movieDate = movieInfo.getString(DATE);
             String movieId = movieInfo.getString(MOVIEID);
-            String splitter = "~#~";
 
-            resultStrArr[i] = new StringBuilder(movieTitle)
-                    .append(splitter)
-                    .append(movieOverview)
-                    .append(splitter)
-                    .append(moviePosterUrl)
-                    .append(splitter)
-                    .append(movieRating)
-                    .append(splitter)
-                    .append(movieDate)
-                    .append(splitter)
-                    .append(movieId)
-                    .toString();
+            resultStrArr[i] =
+                    new String[] {
+                            movieTitle,
+                            movieOverview,
+                            moviePosterUrl,
+                            movieRating,
+                            movieDate,
+                            movieId};
         }
 
-        for(String str : resultStrArr) {
-            Log.v(LOG_TAG, "Movie entry: " + str);
+        for(String[] strArr : resultStrArr) {
+            Log.v(LOG_TAG, "Movie entry: " + strArr.toString());
         }
 
         return resultStrArr;
     }
 
     @Override
-    protected void onPostExecute(String[] result) {
+    protected void onPostExecute(String[][] result) {
         if (result != null) {
             mMovieAdapter.clear();
 
-            for (String s : result) {
-                String[] arr = s.split("~#~");
-                mMovieAdapter.add(new MovieData(arr));
+            for (String[] strArr : result) {
+                mMovieAdapter.add(new MovieData(strArr));
             }
-            //mMovieAdapter.addAll(Movieresult); can't use this because need to split
-            //strings into arrays to
-            //create moviedata objects
-            //analyze parseable
-            //onPostExecute does not allow nested array
+            // mMovieAdapter.addAll(Movieresult);
+            // can't use this because need to
+            // create moviedata objects
+            // analyze parseable
         }
     }
 
     @Override
-    protected String[] doInBackground(String... params) {
+    protected String[][] doInBackground(String... params) {
 
         if (params.length == 0) {
             return null;
